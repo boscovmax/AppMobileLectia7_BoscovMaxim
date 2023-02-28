a00b77c5e179b3e990f9c549da73251ccb840693
PapisDumitru <papis.dumitru@elev.cihcahul.md>
2023-02-28 10:56:50 +0200

    Crearea initiala a proiectului

---
+++ app/src/main/java/com/example/figurigeometrice/Circle.java
@@ -0,0 +1,14 @@
+package com.example.figurigeometrice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Circle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
